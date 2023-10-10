74089a5326bbef3bc40b60892dce458ab7b975b3
GrPugaguga <pugaguga@mail.ru>
2023-10-10 16:33:43 +0500

    Создана страничка курсов и реалезованы запросы на бэк через axios

--- top-app/pages/index.tsx
+++ top-app/pages/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { useState } from 'react';
 import { Htag, Button, P, Tag, Rating } from '../components'
 import { withLayout } from '@/layout/WithLayout';
+import axios from 'axios';
+import { GetStaticProps } from 'next';
+import { MenuItem } from '@/interfaces/menu.interface';
 
 
-
-function Home(): JSX.Element {
+function Home({ menu, firstCategory }): JSX.Element {
   const [rating, setRating] = useState(0)
   return (
     <div>
@@ -19,8 +21,29 @@ function Home(): JSX.Element {
       <Tag size="M" color='green'>q1231we</Tag>
       <Tag size="M" color='grey'>qweerewqr</Tag>
       <Rating rating={rating} isEditable setRating={setRating} />
+      <ul>
+        {menu.map(m => <li key={m._id.secondCategory}>{m._id.secondCategory}</li>)}
+      </ul>
     </div>
   )
 }
 
 export default withLayout(Home)
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0;
+  const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+    firstCategory
+  });
+  return {
+    props: {
+      menu,
+      firstCategory
+    }
+  }
+}
+
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[];
+  firstCategory: number;
+}
